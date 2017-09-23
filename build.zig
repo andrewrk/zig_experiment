b99f2708c10cd57013cbb429a4d6d282457cf7b7
Andrew Kelley <superjoe30@gmail.com>
2017-09-23 13:33:29 -0400

    dynamic library workaround

--- build.zig
+++ build.zig
@@ -2,12 +2,13 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: &Builder) {
   b.addCIncludePath(".");
-  const macos = b.addCObject("macos", "macos.c");
+  const macos = b.addCSharedLibrary("macos", b.version(0, 0, 0));
+  macos.addSourceFile("macos.c");
   macos.addCompileFlags([][]const u8 {
     "-ObjC"
   });
 
   const exe = b.addExecutable("main", "main.zig");
-  exe.addObject(macos);
+  exe.linkLibrary(macos);
   b.default_step.dependOn(&exe.step);
 }
